5c3333fa9fc6b75a416c0eec8c105bc7d8a690a0
Matthew Neil <matthew.neil84@gmail.com>
2016-10-19 13:31:20 +0100

    add frontend files and folder structure

---
+++ JonathanRobbins.xTendingxDB/JonathanRobbins.xTendingxDB/gulpfile.js
@@ -0,0 +1,16 @@
+﻿// gulp require modules
+var gulp = require('gulp');
+
+// less modules 
+var less = require('gulp-less');
+var path = require('path'); // @include path
+
+
+gulp.task('less', function () {
+    return gulp.src('./src/less/*.less')
+    .pipe(less({
+        paths: [path.join(__dirname, 'less', 'modules', 'core')]
+    }))
+    .pipe(gulp.dest('./dist/css'));
+});
+
