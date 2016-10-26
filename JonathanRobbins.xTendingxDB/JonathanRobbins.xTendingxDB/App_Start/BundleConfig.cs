e2b34f67a6e7ccd7d75a8b8ccffcfdb625c0ca75
Matthew Neil <matthew.neil84@gmail.com>
2016-10-26 20:07:55 +0100

    update main nav, update product listing

--- JonathanRobbins.xTendingxDB/JonathanRobbins.xTendingxDB/App_Start/BundleConfig.cs
+++ JonathanRobbins.xTendingxDB/JonathanRobbins.xTendingxDB/App_Start/BundleConfig.cs
@@ -22,12 +22,7 @@ namespace JonathanRobbins.xTendingxDB
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-
-            bundles.Add(new StyleBundle("~/bundles/css").IncludeDirectory("~/Content", "*.css"));
+           
         }
     }
 }
