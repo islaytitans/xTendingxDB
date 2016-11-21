f1ce85138b87382bab5aeb460087aa0572b50ee5
islaytitans <jonathan.m.robbins@gmail.com>
2016-11-21 13:17:02 +0000

    Dev / Initial development of the functionality to index custom elements in custom facets

---
+++ JonathanRobbins.xTendingxDB/JonathanRobbins.xTendingxDB.Index/Facets/Contacts/IndexSampleOrders.cs
@@ -0,0 +1,35 @@
+﻿using JonathanRobbins.xTendingxDB.CMS.xDB.Facets;
+using JonathanRobbins.xTendingxDB.CMS.xDB.Interfaces.Facets;
+using Sitecore.Analytics.Model.Entities;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Analytics.Pipelines.ContactIndexableLoadFields;
+using Sitecore.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JonathanRobbins.xTendingxDB.Index.Facets.Contacts
+{
+    public class IndexSampleOrders : ContactIndexableLoadFieldsProcessor
+    {
+        protected override IEnumerable<IIndexableDataField> GetFields(ContactIndexableLoadFieldsPipelineArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            IContact contact = args.Contact;
+            Assert.IsNotNull(contact, "contact");
+            var fields = new List<IIndexableDataField>();
+            var keyInteractionsFacet = contact.GetFacet<IKeyInteractionsFacet>(KeyInteractionsFacet.FacetName);
+            if (keyInteractionsFacet != null)
+            {
+                var skus = (from s in keyInteractionsFacet.SamplesOrdered
+                    select s.Sku).Distinct();
+                var types = (from s in keyInteractionsFacet.SamplesOrdered
+                    select s.Type).Distinct();
+
+
+                fields.Add(new IndexableDataField<string>("contact.SampleOrder.Skus", string.Join(",", skus)));
+                fields.Add(new IndexableDataField<string>("contact.SampleOrder.Types", string.Join(",", types)));
+            }
+            return fields;
+        }
+    }
+}
